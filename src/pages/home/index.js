e3012788a0ede643e658f8916f51c4ae432471ed
Valdecir Teixeira <vardeba@hotmail.com>
2022-11-03 20:30:35 -0300

    feat: home finished

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,8 +2,6 @@ import { getAllCompanies, getAllSectors, filterCompanies } from "../../scripts/r
 
 import { createCompanyToRender, createSectorToRender } from "../../scripts/renders.js";
 
-// getAllCompanies();
-
 const renderCompanies = async () => {
     const allCompanies = await getAllCompanies();
     const ulCompanies = document.querySelector('.companies');
@@ -39,6 +37,19 @@ const renderFilteredCompanies = async () => {
 
     });
 }
+
+const hamburgerMenu = async () => {
+    const hamburgerButton = document.querySelector('#hamburger');
+    const buttonsContainer = document.querySelector('.btns_header_containner');
+    hamburgerButton.addEventListener('click', () => {
+        hamburgerButton.classList.toggle('show_hamburger');
+        hamburgerButton.classList.toggle('hide_hamburger');
+        buttonsContainer.classList.toggle('show_menu');
+        buttonsContainer.classList.toggle('hide_menu');
+    })
+};
+
+await hamburgerMenu();
 
 await renderCompanies();
 
